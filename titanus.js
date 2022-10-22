2158672ad851f0080414cd27f28303daf6086474
ostov-larion <39136591+ostov-larion@users.noreply.github.com>
2022-10-22 16:20:04 +0500

    Add files via upload

---
+++ titanus.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+let gemini = require("gemini-server").default
+let { execFileSync } = require('child_process')
+let { readFileSync } = require('fs')
+let args = require('args')
+
+let flags = {
+    cert   : process.argv[2],
+    key    : process.argv[3],
+    domain : process.argv[4]
+}
+
+console.log(flags)
+
+let options = {
+    cert: readFileSync(flags.cert || "cert.pem"),
+    key: readFileSync(flags.key   || "key.pem"),
+    titanEnabled: true
+}
+
+let app = gemini(options)
+
+app.titan('/:cgi', (req, res) => {
+    try {
+        execFileSync(`./${req.params.cgi}`, [req.data.toString('utf8').replace(/\n/g,'\\n')])
+    }
+    catch(e) {
+        console.log('TITAN ERROR: CGI error.')
+        res.error(52, "This route not found.")
+        return
+    }
+    res.redirect(`gemini://${flags.domain}/${req.params.cgi}.gmi`)
+})
+
+app.listen(1917)
